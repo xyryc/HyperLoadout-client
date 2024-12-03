aa605918a0d0562753c578dcab3a493a9c601eb0
xyryc <amdanikzz@gmail.com>
2024-12-03 21:33:48 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,72 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-red-600 font-bold underline" : ""
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+      </li>
+
+      <li>
+        <a>All Equipments</a>
+      </li>
+
+      <li>
+        <a>Add Equipment</a>
+      </li>
+
+      <li>
+        <a>My Equipment List</a>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="font-bebas-neue tracking-wider">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">HyperLoadout</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
